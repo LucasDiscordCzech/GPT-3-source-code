749fbc8798a03809cc630168b0994a74280eee6f
LucasDiscordCzech <98537331+LucasDiscordCzech@users.noreply.github.com>
2023-03-25 23:33:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,63 @@
-// This is just an example of the source code, please modify as you like.
+const { Client, Events, GatewayIntentBits } = require("discord.js")
+require("dotenv/config")
+const { OpenAIApi, Configuration } = require("openai")
 
-// Import the required libraries
-const Discord = require('discord.js');
-const { createOpenAI } = require('@openai/openai-api');
+const config = new Configuration({
+    apiKey: process.env.OPENAI_KEY
+})
 
-// Create a new Discord client
-const client = new Discord.Client();
+const openai = new OpenAIApi(config)
 
-// Create a new OpenAI instance
-const openai = createOpenAI(process.env.OPENAI_API_KEY);
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
 
-// Listen for the 'ready' event
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+client.once(Events.ClientReady, (clientUser) => {
+    console.log(`Logged in as ${clientUser.user.tag}`)
+})
 
-// Listen for the 'interactionCreate' event
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
+client.login(process.env.BOT_TOKEN)
 
-  if (interaction.commandName === 'chat') {
-    const query = interaction.options.getString('query');
-    
-    // Make an API request to OpenAI
-    const response = await openai.complete({
-      engine: 'davinci',
-      prompt: query,
-      maxTokens: 150,
-      temperature: 0.7,
-    });
-    
-    // Get the generated text from the API response
-    const message = response.data.choices[0].text;
-    
-    // Send the generated text to the Discord channel where the command was used
-    await interaction.reply(message);
-  }
-});
+const BOT_CHANNEL = "1067560640526438510"
+const PAST_MESSAGES = 5
 
-// Log in to the Discord client
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.on(Events.MessageCreate, async (message) => {
+    if (message.author.bot) return
+    if (message.channel.id !== BOT_CHANNEL) return
+
+    message.channel.sendTyping()
+
+    let messages = Array.from(await message.channel.messages.fetch({
+        limit: PAST_MESSAGES,
+        before: message.id
+    }))
+    messages = messages.map(m=>m[1])
+    messages.unshift(message)
+
+    let users = [...new Set([...messages.map(m=> m.member.displayName), client.user.username])]
+
+    let lastUser = users.pop()
+
+    let prompt = `The following is a conversation between ${users.join(", ")}, and ${lastUser}. \n\n`
+
+    for (let i = messages.length - 1; i >= 0; i--) {
+        const m = messages[i]
+        prompt += `${m.member.displayName}: ${m.content}\n`
+    }
+    prompt += `${client.user.username}:`
+    console.log("prompt:", prompt)
+
+    const response = await openai.createCompletion({
+        prompt,
+        model: "text-davinci-003",
+        max_tokens: 500,
+        stop: ["\n"]
+    })
+
+    console.log("response", response.data.choices[0].text)
+    await message.channel.send(response.data.choices[0].text)
+})
